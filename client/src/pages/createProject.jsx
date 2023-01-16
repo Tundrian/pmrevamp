e36db10eadf84bcfc3af267bb6ea69f04dd5bd6c
Antonio <antoniosansotta@gmail.com>
2023-01-15 19:23:44 -0500

    added dropdown arrow to project create form

--- client/src/pages/createProject.jsx
+++ client/src/pages/createProject.jsx
@@ -16,16 +16,18 @@ function createProject() {
             </label>
             <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Customer Name" />
           </div>
-          <div className="mb-4 flex">
-          <label className="block text-gray-700 text-xl px-3 mb-2" htmlFor="username">
-              Modules
-          </label>
-          <select name="" id="" className="bg-white shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
-            {modules.length ? modules.map((module,i) =>
-              <option key={module} onClick={() => setModulesChosen([...modulesChosen,module])} value={module}>{module}</option>             
-            ) : ''}
-          </select>
-
+          <div className="mb-4 flex relative">
+            <label className="block text-gray-700 text-xl px-3 mb-2" htmlFor="username">
+                Modules
+            </label>
+            <select name="" id="" className="bg-white shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+              {modules.length ? modules.map((module,i) =>
+                <option key={module} onClick={() => setModulesChosen([...modulesChosen,module])} value={module}>{module}</option>             
+              ) : ''}
+            </select>
+            <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+          <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+        </div>
           </div>
           {modulesChosen.length ? (
             <section className="bg-white shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mb-3">
