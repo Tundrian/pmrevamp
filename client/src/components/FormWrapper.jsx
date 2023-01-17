a7a9e961c5c3ff13c507df59e6a067bdcdf085a7
Antonio <antoniosansotta@gmail.com>
2023-01-16 19:53:00 -0500

    added multi-form field functionality and initial template

---
+++ client/src/components/FormWrapper.jsx
@@ -0,0 +1,12 @@
+/*
+    type FormWrapperPropers = {
+        title: string
+        children: ReactNode
+    }
+*/  
+export function FormWrapper({title, children }){
+    return <>
+        <h2 className="text-center m-0 mb-[2rem]">{title}</h2>
+        <div className="grid gap-[1rem] justify-start grid-cols-2">{children}</div>
+    </>
+}
