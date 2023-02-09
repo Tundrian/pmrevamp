e7123c8c74cd8074d5e74ef7caba38804ac66477
Antonio <antoniosansotta@gmail.com>
2023-02-08 20:19:35 -0500

    fully integrated backend routes for steps to frontend client module configuration page

--- client/src/lists/modules.js
+++ client/src/lists/modules.js
@@ -1,4 +1,5 @@
 export const modules = [
+    '',
     "Company",
     "Platform Services",
     "General Ledger",
--- client/src/pages/ConfigureModules.jsx
+++ client/src/pages/ConfigureModules.jsx
@@ -4,60 +4,60 @@ import { implementationTypes } from '../lists/implementationTypes'
 import { Card, Form, Button } from 'react-bootstrap'
 import { Typeahead } from 'react-bootstrap-typeahead'
 
-let testData = [
-    {
-        id: '1',
-        name: 'Grant Access',
-        description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Necessitatibus, nemo!',
-        module: 'Company',
-        type: 'Configuration',
-        category: 'External Authorization',
-        notes: '',
-        answer: '',
-        link: '',
-        status: '',
-        attachment: '',
-    },
-    {
-        id: '2',
-        name: 'Set up Admin Role',
-        description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Necessitatibus, nemo!',
-        module: 'Company',
-        type: 'Configuration',
-        category: 'External Authorization',
-        notes: '',
-        answer: '',
-        link: '',
-        status: '',
-        attachment: '',
-    },
-    {
-        id: '3',
-        name: 'Import COA',
-        description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Necessitatibus, nemo!',
-        module: 'General Ledger',
-        type: 'Import',
-        category: 'Setup Import',
-        notes: '',
-        answer: '',
-        link: '',
-        status: '',
-        attachment: '',
-    },
-    {
-        id: '4',
-        name: 'Setup entities',
-        description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Necessitatibus, nemo!',
-        module: 'Company',
-        type: 'Configuration',
-        category: 'Entities',
-        notes: '',
-        answer: '',
-        link: '',
-        status: '',
-        attachment: '',
-    },
-]
+// let testData = [
+//     {
+//         id: '1',
+//         name: 'Grant Access',
+//         description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Necessitatibus, nemo!',
+//         module: 'Company',
+//         type: 'Configuration',
+//         category: 'External Authorization',
+//         notes: '',
+//         answer: '',
+//         link: '',
+//         status: '',
+//         attachment: '',
+//     },
+//     {
+//         id: '2',
+//         name: 'Set up Admin Role',
+//         description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Necessitatibus, nemo!',
+//         module: 'Company',
+//         type: 'Configuration',
+//         category: 'External Authorization',
+//         notes: '',
+//         answer: '',
+//         link: '',
+//         status: '',
+//         attachment: '',
+//     },
+//     {
+//         id: '3',
+//         name: 'Import COA',
+//         description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Necessitatibus, nemo!',
+//         module: 'General Ledger',
+//         type: 'Import',
+//         category: 'Setup Import',
+//         notes: '',
+//         answer: '',
+//         link: '',
+//         status: '',
+//         attachment: '',
+//     },
+//     {
+//         id: '4',
+//         name: 'Setup entities',
+//         description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Necessitatibus, nemo!',
+//         module: 'Company',
+//         type: 'Configuration',
+//         category: 'Entities',
+//         notes: '',
+//         answer: '',
+//         link: '',
+//         status: '',
+//         attachment: '',
+//     },
+// ]
 
 export function ConfigureModules() {
     const [fields, setFields] = useState({
@@ -72,42 +72,35 @@ export function ConfigureModules() {
         notes: '',
         status: '',
     })
+    const [dbData, setDbData] = useState([])
 
     const uniqueTypes = useRef([])
     const uniqueCategories = useRef([])
     const stepsList = useRef([])
-    const [dbData, setDbData] = useState([])
-
-    let options = [
+    
+    const options = [
+        '',
         'Company Configuration',
         'Admin',
         'External Authorization',
     ]
 
-    function submitForm() {
-        // console.log(fields)
-        testData.push(fields)
-        options.push(fields.category)
-        updateList(fields.module)
+    async function submitForm() {
+        console.log('submitForm')
+        await pushData()
+        await getData()
+        await updateList(fields.module)
     }
-
-    // useEffect(() => {
-    //     // updateList(fields.module || 'Company')
-    // }, [])
-
 
     async function getData(){
+        console.log('getData')
         const request = await fetch(`http://localhost:5000/api/step`)
         const data = await request.json()
-        return data
+        setDbData(data)
     }
 
-    useEffect(()=>{
-        setDbData((prevData) => getData())    
-    }, [fields.module])
-
     async function pushData(){
-        console.log({...fields})
+        console.log('pushData')
         const request = await fetch(`http://localhost:5000/api/step`,{
             method: 'POST',
             headers: {
@@ -117,16 +110,18 @@ export function ConfigureModules() {
             body: JSON.stringify(fields)
         })
         const data = await request.json()
-        console.log(data)
+        console.log('data push: ', data)
     }
 
     async function updateList(module){
+ 
+        console.log('updateList')
+        console.log("dbData: ", dbData)
+        console.log('module: ', module)
 
-        // setFields({ ...fields, module: module })
-        await pushData()
-        await getData()
-
-        stepsList.current = await dbData.filter(step => step.module === module)
+        setFields({...fields, module: module})
+        
+        stepsList.current = dbData.filter(step => step.module === module)
 
         uniqueTypes.current = Array.from(new Set(stepsList.current.map(step => step.type)))
 
@@ -136,11 +131,15 @@ export function ConfigureModules() {
 
         }, new Map()).values()]
 
-        // console.log(uniqueCategories)
+        console.log('stepsList: ', stepsList.current)
+        console.log('uniqueTypes: ', uniqueTypes.current)
+        console.log('uniqueCategories: ', uniqueCategories.current)
+    
     }
 
     return (
         <>
+            <Button onClick={() => getData()}>Get Data</Button>
             <Card className="m-3">
                 <Card.Body>
                     <Form>
@@ -211,7 +210,6 @@ export function ConfigureModules() {
                     <Button onClick={() => submitForm()}>Submit</Button>
                 </Card.Footer>
             </Card>
-
             <Card className="m-3">
                 <Card.Header>Existing Steps</Card.Header>
                 <Card.Body>
@@ -226,7 +224,7 @@ export function ConfigureModules() {
                                             <ul>
                                             {stepsList.current.filter(step => step.category === category.category && step.type === type).map(step => {
                                                 return (
-                                                    <li key={step.id}>{step.name}</li>
+                                                    <li key={step._id}>{step.task}</li>
                                                 )
                                             })}
                                             </ul>
